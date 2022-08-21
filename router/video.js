b8495992d9ef255b015ec130c68461d30446862c
Xinli WU <ryan7gm@gmail.com>
2022-08-21 17:33:00 +1000

    chunk support

--- router/video.js
+++ router/video.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 const { DateTime } = require("luxon");
 
 // middleware that is specific to this router
@@ -13,7 +14,35 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res, next) => {
-  res.sendFile('./assets/001.mp4', { root: __dirname });
+  const videoPath = `assets/${req.params.id}.mp4`;
+  const videoStat = fs.statSync(videoPath);
+  const fileSize = videoStat.size;
+  const videoRange = req.headers.range;
+  if (videoRange) {
+    const parts = videoRange.replace(/bytes=/, "").split("-");
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    console.log({ start, end });
+    const chunksize = (end - start) + 1;
+
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    };
+    res.writeHead(206, head);
+
+    const videoStream = fs.createReadStream(videoPath, { start, end });
+    videoStream.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(videoPath).pipe(res);
+  }
 });
 
 
