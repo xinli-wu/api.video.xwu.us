aca0ffb8ba110db1e8fab30542eaa647a62692fc
Xinli WU <ryan7gm@gmail.com>
2022-08-24 01:21:21 +1000

    cors update

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const videoRoute = require('./router/video');
+const cors = require('cors');
+
 const app = express();
 const port = 4000;
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://video.xwu.us']
+}));
+
 app.use('/video', videoRoute);
 
 app.listen(port, () => {
