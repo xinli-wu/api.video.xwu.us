3ed149ecb2a62a30a1b240c8cd61b25fd373ef6c
Xinli WU <ryan7gm@gmail.com>
2022-08-31 01:21:16 +1000

    test

--- router/yt.js
+++ router/yt.js
@@ -34,7 +34,7 @@ router.use(async (req, res, next) => {
     req['youtube'] = { q };
   }
 
-  res.set('Cache-control', 'public, max-age=7200');
+  res.set('Cache-control', 'public, max-age=3600');
   next();
 });
 
